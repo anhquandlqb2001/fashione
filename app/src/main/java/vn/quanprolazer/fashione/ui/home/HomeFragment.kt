b301f428b0c9dd9acfb36b9c3dc2eeebaeef6792
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-03-26 20:37:10 +0700

    home_ui

--- app/src/main/java/vn/quanprolazer/fashione/ui/home/HomeFragment.kt
+++ app/src/main/java/vn/quanprolazer/fashione/ui/home/HomeFragment.kt
@@ -1,7 +1,7 @@
 package vn.quanprolazer.fashione.ui.home
 
+import android.graphics.Rect
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +9,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import vn.quanprolazer.fashione.R
 import vn.quanprolazer.fashione.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,32 +23,94 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
+        // Inflate layout
         binding = FragmentHomeBinding.inflate(inflater)
 
         val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
+        // Category Section
         val categoryAdapter = CategoryAdapter()
-
         binding.rvCategory.adapter = categoryAdapter
-
-        val productAdapter = ProductAdapter()
-
-        binding.rvFeatured.adapter = productAdapter
-
         homeViewModel.categories.observe(viewLifecycleOwner, Observer {
             it?.let {
                 categoryAdapter.submitList(it)
             }
         })
+        // End Category Section
 
+        // Product Featured Section
+        val productFeaturedAdapter = ProductAdapter()
+        binding.rvFeatured.adapter = productFeaturedAdapter
         homeViewModel.products.observe(viewLifecycleOwner, Observer {
             it?.let {
-                productAdapter.submitList(it)
+                productFeaturedAdapter.submitList(it)
             }
         })
+        // End Product Featured Section
+
+
+        // Product Best Sell Section
+        val productBestSellAdapter = ProductAdapter()
+        binding.rvBestSell.adapter = productBestSellAdapter
+        homeViewModel.products.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                productBestSellAdapter.submitList(it)
+            }
+        })
+        // End Product Best Sell Section
+
+        // Product Suggest Section
+        val productSuggestAdapter = ProductAdapter()
+        binding.rvSuggestProduct.adapter = productBestSellAdapter
+        homeViewModel.products.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                productSuggestAdapter.submitList(it)
+            }
+        })
+
+        binding.rvSuggestProduct.addItemDecoration(
+            MarginItemDecoration(resources.getDimensionPixelSize(R.dimen.margin), 2)
+        )
+
+        val productSuggestLayoutManager = GridLayoutManager(context, 2)
+        binding.rvSuggestProduct.layoutManager = productSuggestLayoutManager
+        // End Product Suggest Section
 
         return binding.root
 
     }
 
+}
+
+
+class MarginItemDecoration(
+    private val spaceSize: Int,
+    private val spanCount: Int = 1,
+    private val orientation: Int = GridLayoutManager.VERTICAL
+) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect, view: View,
+        parent: RecyclerView, state: RecyclerView.State
+    ) {
+        with(outRect) {
+            if (orientation == GridLayoutManager.VERTICAL) {
+                if (parent.getChildAdapterPosition(view) < spanCount) {
+                    bottom = spaceSize
+                }
+                if (parent.getChildAdapterPosition(view) % spanCount == 0) {
+                    right = spaceSize
+                }
+            } else {
+                if (parent.getChildAdapterPosition(view) < spanCount) {
+                    right = spaceSize
+                }
+                if (parent.getChildAdapterPosition(view) % spanCount == 0) {
+                    bottom = spaceSize
+                }
+            }
+
+            top = spaceSize
+            left = spaceSize
+        }
+    }
 }
