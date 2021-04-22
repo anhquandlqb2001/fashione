32ac67cc77d3fe473bfd5f37b78e80a7731b7fb7
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-04-22 23:23:20 +0700

    update_cart_item_ui

--- app/src/main/java/vn/quanprolazer/fashione/utilities/CartBindingAdapter.kt
+++ app/src/main/java/vn/quanprolazer/fashione/utilities/CartBindingAdapter.kt
@@ -9,6 +9,7 @@ package vn.quanprolazer.fashione.utilities
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import vn.quanprolazer.fashione.data.domain.model.ProductImage
 import vn.quanprolazer.fashione.data.domain.model.Result
@@ -37,4 +38,14 @@ fun Button.addToCartVisible(orderQty: Number?) {
             View.INVISIBLE
         }
     }
+}
+
+@BindingAdapter(value = ["variantName", "variantValue"])
+fun TextView.variantOptionName(variantName: String?, variantValue: String?) {
+    if (variantName == null || variantValue == null) return
+
+    val texToDisplay = "Loại: $variantName - $variantValue"
+
+    text = texToDisplay.smartTruncate(20)
+
 }
