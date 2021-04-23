9577ae45370c8193ef80bb7c3ad600a94faa90e2
dongle.qb.2001@gmail.com <dongle.qb.2001@gmail.com>
2021-04-23 09:34:12 +0700

    add_to_cart_check_exist

--- app/src/main/java/vn/quanprolazer/fashione/data/network/service/OrderServiceImpl.kt
+++ app/src/main/java/vn/quanprolazer/fashione/data/network/service/OrderServiceImpl.kt
@@ -6,21 +6,35 @@
 
 package vn.quanprolazer.fashione.data.network.service
 
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firestore.v1.WriteResult
 import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import vn.quanprolazer.fashione.data.domain.model.AddToCartItem
 import vn.quanprolazer.fashione.data.domain.model.Result
 import vn.quanprolazer.fashione.data.network.dto.NetworkCartItem
 import vn.quanprolazer.fashione.data.network.mapper.toHashMap
+
+
 class OrderServiceImpl : OrderService {
     override suspend fun addToCart(addToCartItem: AddToCartItem, userId: String): Result<Boolean> {
         val db = FirebaseFirestore.getInstance()
         return try {
-            db.collection("carts")
-                .add(addToCartItem.toHashMap())
-                .await()
+            val existCartItem = db.collection("carts").whereEqualTo(
+                "variant_option_id", addToCartItem.variantOptionId
+            ).get().await().documents
 
+            // if cart item has exist
+            if (existCartItem.size != 0) {
+                val washingtonRef = db.collection("carts").document(existCartItem[0].id)
+                washingtonRef.update(
+                    "quantity", FieldValue.increment(addToCartItem.quantity.toLong())
+                )
+                return Result.Success(true)
+            }
+
+            db.collection("carts").add(addToCartItem.toHashMap()).await()
             Result.Success(true)
         } catch (e: Exception) {
             Timber.e(e)
@@ -31,12 +45,8 @@ class OrderServiceImpl : OrderService {
     override suspend fun getCartItems(userId: String): Result<List<NetworkCartItem>> {
         val db = FirebaseFirestore.getInstance()
         return try {
-            val response = db.collection("carts")
-                .whereEqualTo("user_id", userId)
-                .get()
-                .await()
-                .documents
-                .mapNotNull { it.toObject(NetworkCartItem::class.java) }
+            val response = db.collection("carts").whereEqualTo("user_id", userId).get()
+                .await().documents.mapNotNull { it.toObject(NetworkCartItem::class.java) }
 
             Result.Success(response)
         } catch (e: Exception) {
